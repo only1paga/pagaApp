839c18128f5799d9a79d80040d35a67b3f30700e
only1paga <only1paga@gmail.com>
2026-06-18 17:30:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StartUp from "./pages/StartUp";
 import Home from "./pages/Home";
 import Pin from "./pages/Pin";
 import Otp from "./pages/Otp";
@@ -8,7 +9,8 @@ function App() {
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<StartUp />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/pin" element={<Pin />} />
           <Route path="/otp" element={<Otp />} />
         </Routes>
